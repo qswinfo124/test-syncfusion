d2e485527cf54537604680cfbfc02787ee322c45
Admin <alan.qin@infoflosolutions.com>
2020-02-19 14:54:47 -0500

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FroalaEditorModule, FroalaViewModule} from 'angular-froala-wysiwyg';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
